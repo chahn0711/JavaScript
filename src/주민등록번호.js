b2b22291827c6d51f0f87f37b01c667c05bfbc54
chahn0711 <chahn0711@naver.com>
2023-10-18 17:40:29 +0900

    20231018오후

---
+++ src/주민등록번호.js
@@ -0,0 +1,46 @@
+﻿/* 
+문자열을 자르는 substring사용
+날짜 계산을 위해 Date 내장 객체 사용 (new Date())
+주민등록번호 입력 길이 체크
+하이픈 포함 여부 체크 (indexOf)
+HTML + CSS로 이쁘게 출력 하기 
+
+필요한 거
+prompt
+주민번호 아닌거 걸러내기 alert, while, break, length(길이 찾기)
+성별 구하는 공식
+나이 구하는 공식
+성별 나이 구하려면 
+숫자 골라낼 인덱스 필요 indexOf
+날짜 계산 : 나이 구하기 위해서
+console.log(`나이: ${}`);
+
+
+*/
+
+
+let jumin;
+while(true) {
+    jumin = prompt("주민등록번호 : ", "");
+    if(jumin.length != 14) alert("입력 오류 : 다시 입력 하세요.");
+    else {
+        if(jumin.indexOf("-") == -1) alert("하이픈이 누락되었습니다");
+        else break;
+    }
+}
+let gender = jumin.charAt(7); // 문자열에서 해당 인덱스 문자를 추출
+let gerderStr;
+if (gender == "1" || gender == "3") gerderStr = "남성";
+else gerderStr = "여성";
+
+let birthYear = jumin.substring(0, 2); // 주민등록번호에서 맨앞에 2자를 잘라냄
+if(gender == "1" || gender == "2") { // gender가 1 또는 2이면 19xx년에 출생
+    birthYear = "19" + birthYear;
+} else birthYear = "20" + birthYear; // 20xx년도 출생
+birthYear = Number(birthYear); // 계산을 위해 태어날 년도 문자열을 숫자로 변경
+
+const date = new Date(); // 날짜 객체 만들기
+const currYear = date.getFullYear(); // 2022
+let age = currYear - birthYear; // 현재 년도에서 태어난 년도 빼면 나이가 됨
+
+document.write("<p class='box'> 성별 : " + gerderStr + "<br>" + "나이 : " + age + "</p>");
---
+++ src/표준내장객체.js
@@ -0,0 +1,65 @@
+﻿/* 표준 내장 객체: 자바스트립트에서 개발의 편의를 위해 미리 만들어 놓은 것
+문자열 다루는 String 객체
+length : 메서드가 아닌 속성으로 존재, 문자열의 길이를 반환 */
+
+const pw = "1234";
+if(pw.length < 6) {
+    console.log("비밀번호의 길이는 최소 6자리 이상 입니다.");
+    console.log(`입력 받은 비밀번호 길이는 ${pw.length}`);
+} else {
+    console.log("비밀번호 길이가 적절 합니다.");
+}
+// 특정 문자열 포함 여부 확인 (true/false로 결과값 반환)
+const email = "test!naver.com";
+if(email.includes("@") === false) {
+    console.log("올바른 메일 형식이 아닙니다.");
+}
+// 대상 문자열과 일치하는 첫 번째 문자의 인덱스를 반환, 찾지 못하면 -1을 반환
+const email2 = "test!naver.com";
+if(email2.indexOf("@") === -1) {
+    console.log("올바른 메일 형식이 아닙니다.");
+}
+
+// lastIndexOf() : 찾고자하는 문자열이 둘 이상 발견되면 마지막에 발견된 문자열의 index를 반환
+const email3 = "test!na!ver!.com";
+if(email3.lastIndexOf("!") !== -1) {
+    console.log(email3.indexOf("!"));
+}
+// slice() : 시작위치와 종료 위치가 주어지면, 문자열에서 해당 부분을 잘라내서 반환
+const str = "Apple, Banana, Kiwi";
+const rst = str.slice(7);
+const newStr = str.replace("Kiwi","Orange"); // 문자열을 변경
+console.log(newStr); // "Apple, Banana, Kiwi" -> "Apple, Banana, Orange"
+
+// concat() : 2개 이상의 문자열을 하나의 문자열로 합치는 함수이며, 기존의 문자열을 변경하지 않음
+const text1 = "Hello";
+const text2 = "World";
+const text3 = text1.concat(" " + text2);
+console.log(text3); // Hello World
+
+// padStart(), padEnd()
+let str40 = "5";
+str40 = str40.padStart(4,0); // 0005
+console.log(str40);
+
+let str50 = "HELLO WORLD";
+console.log(str50.charCodeAt(0));
+
+let birthday = "1997-06-12";
+let arr2 = birthday.split("-"); // 하이픈(-)을 기준으로 문자열을 분리해서 배열로
+// console.log(arr2); // ["1997", "06", "02"]
+
+// Number 객체 : 숫자와 관련된 작업을 할 때 사용하는 메소드
+// Number.parseFloat() 메소드 : 문자열에 포함된 숫자 부분을 실수 형태로 반환
+console.log(Number.parseFloat("12"));         // 12
+console.log(Number.parseFloat("12.34"));      // 12.34
+console.log(Number.parseFloat("12문자열"));   // 12
+console.log(Number.parseFloat("12 34 56"));   // 12
+console.log(Number.parseFloat("문자열 56")); // NaN
+
+// Number.parseInt() 메소드
+Number.parseInt("12");         // 12
+Number.parseInt("12.34");      // 12
+Number.parseInt("12문자열");   // 12
+Number.parseInt("12 34 56");   // 12
+Number.parseInt("문자열 56"); // NaN
