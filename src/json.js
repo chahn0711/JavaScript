cfc8d17feffea4896421aabac1a2d9448b0da68f
chahn0711 <chahn0711@naver.com>
2023-10-23 12:43:23 +0900

    2023-10-23

---
+++ src/json.js
@@ -0,0 +1,27 @@
+const memberInfo = [
+    {
+        name:"이영지",
+        age:22,
+        job:"래퍼",
+        addr: "서울시 강남구 청담동",
+    },
+    {
+        name: "미미",
+        age:25,
+        job:"오마이걸",
+        addr:"서울시 강남구 역삼동",
+    },
+    {
+        name:"안유진",
+        age:21,
+        job:"아이브",
+        addr:"서울시 강북구",
+    }, // 다음 값이 없어도 끝에 ,는 관례
+];
+// 자바스크립트 객체를 JSON 문자열로 변환 : stringify
+const json = JSON.stringify(memberInfo);
+console.log(json);
+
+// JSON 문자열을 자바스크립트 객체로 변환 : parse
+const jsObject = JSON.parse(json);
+console.log(jsObject);
